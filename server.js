6a4172b8c5d3bdb5d86593b35acd8145963adfd1
Chirag Khatri <chiragkhatri2001@gmail.com>
2022-06-22 01:10:17 +0530

    initial route file ready

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// route files
+const bootcamps = require("./routes/bootcamps");
+
+//load env variables
 dotenv.config();
 
 const app = express();
 
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
-
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Get bootcamp ${req.params.id} ` });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id} ` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
+//mount routes
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, console.log("server running"));
